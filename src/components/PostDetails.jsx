a29b79147053d5fc3285747d98437e844282d4bb
marco9727 <marco.albarella97@gmail.com>
2024-04-29 17:04:24 +0200

    commit aggiunta pag dateils post

--- src/components/PostDetails.jsx
+++ src/components/PostDetails.jsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom/dist";
+import { baseApiUrl } from "../constants";
+
 const Details = () => {
-  return <h1>ciao</h1>;
+  const [post, setPost] = useState(null);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`${baseApiUrl}/posts/${id}`).then((response) =>
+      response.json().then((data) => {
+        console.log(data);
+        setPost(data);
+      })
+    );
+  }, []);
+
+  return (
+    post && (
+      <div>
+        <h1>{post.title.rendered}</h1>
+        {/* questo div serve per mostrare il contenuto dell'articolo visto che 
+        senza inserire questo codice  non si vedrebbero i dati voluti */}
+        <div dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>
+      </div>
+    )
+  );
 };
 
 export default Details;
